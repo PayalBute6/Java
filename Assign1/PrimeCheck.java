7b0bd2e877ac2ac06fa3017f2882be1cc6ff5099
Payal <payalbute6@gmail.com>
2026-08-17 21:32:23 +0530

    Commited 17/08/2026

---
+++ Assign1/PrimeCheck.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        // Count how many numbers divide 'num' completely
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        // A prime number has EXACTLY 2 factors (1 and itself)
+        if (count == 2) {
+            System.out.println(num + " is a Prime Number.");
+        } else {
+            System.out.println(num + " is NOT a Prime Number.");
+        }
+
+        sc.close();
+    }
+}
