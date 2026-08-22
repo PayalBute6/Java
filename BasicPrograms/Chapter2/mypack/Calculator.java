ac0a3c88f45beb803eafe08cf9404e1f2bf07e94
Payal <payalbute6@gmail.com>
2026-08-22 22:22:28 +0530

    Commit 22/08/2026

---
+++ BasicPrograms/Chapter2/mypack/Calculator.java
@@ -0,0 +1,25 @@
+// Topic 2.7: Package Creation
+// File: BasicPrograms/Chapter2/mypack/Calculator.java
+package mypack;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero!");
+            return 0;
+        }
+        return a / b;
+    }
+}
