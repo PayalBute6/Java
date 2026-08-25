70a9cd6e0a7e8b89fba5afe2795d7165b388fc07
Payal <payalbute6@gmail.com>
2026-08-25 23:15:53 +0530

    Commit 25/08/2026

---
+++ JavaPrep/ReverseTheNumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ReverseTheNumber{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+        int rev = 0;
+        while(n>0){
+            int lastDigit = n%10;
+            rev = rev*10 + lastDigit;
+            n = n/10;
+        }
+        System.out.println("The reversed number is: " + rev);
+    }
+}
