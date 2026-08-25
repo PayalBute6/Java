dea644e47a8e2a69ab871cc1e44877f3dde090ae
Payal <payalbute6@gmail.com>
2026-08-25 23:09:38 +0530

    Commit 25/08/2026

--- JavaPrep/MultiplicationTable.java
+++ JavaPrep/MultiplicationTable.java
@@ -1,13 +1,18 @@
 import java.util.*;
 
-class MultiplicationTable{
-    public static void main(String[] args) {
+class MultiplicationTable
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter a number: ");
+
         int n = sc.nextInt();
-        for(int i=1; i<=10; i++){
-            System.out.println(n+"*"+i+"="+n*i);
+
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(n + "*" + i + "=" + n * i);
         }
     }
-
 }
