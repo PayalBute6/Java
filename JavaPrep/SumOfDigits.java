338227aef5a2a2427852259d5ba2481310bad0b2
Payal <payalbute6@gmail.com>
2026-08-25 22:43:28 +0530

    Commit 25/08/2026

--- JavaPrep/SumOfDigits.java
+++ JavaPrep/SumOfDigits.java
@@ -1,6 +1,15 @@
+import java.util.*;
 public class SumOfDigits{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter digit: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        while(n>0){
+            int lastDigit = n%10;
+            sum = sum + lastDigit;
+            n = n/10;
+        }
+        System.out.println("The sum of the digits is: " + sum);
     }
 }
