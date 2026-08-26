2693ecc4d2953f1f2bf1095ada8bc54902b1d446
Payal <payalbute6@gmail.com>
2026-08-26 20:42:32 +0530

    Commited 26/08/2026

---
+++ JavaPrep/PalindromeNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class PalindromeNumber{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int temp = num;
+        int rev = 0;
+        while(temp > 0){
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp = temp / 10;
+        }
+        if(num == rev){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
+    }
+}
---
+++ JavaPrep/PalindromeString.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a word/string: ");
+            String str = sc.next();
+            
+            // Method 1: Two-Pointer approach (Efficient)
+            boolean isPalindrome = true;
+            int left = 0;
+            int right = str.length() - 1;
+            
+            while (left < right) {
+                if (str.charAt(left) != str.charAt(right)) {
+                    isPalindrome = false;
+                    break;
+                }
+                left++;
+                right--;
+            }
+            
+            if (isPalindrome) {
+                System.out.println(str + " is a Palindrome.");
+            } else {
+                System.out.println(str + " is NOT a Palindrome.");
+            }
+        }
+    }
+}
