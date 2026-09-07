5fc9fddb73691a9bae554f813e9d321224d7953e
Payal <payalbute6@gmail.com>
2026-09-07 19:46:41 +0530

    Commit 07/09/2026

---
+++ mypack/Test.java
@@ -0,0 +1,10 @@
+import mypack.Greeting; // Import specific class
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Greeting g = new Greeting();
+        g.showMsg();
+    }
+}
