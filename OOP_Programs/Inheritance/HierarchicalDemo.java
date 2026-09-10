3ac1d8eed65828e60b3164aecc085f9a7e224c83
Payal <payalbute6@gmail.com>
2026-09-10 20:22:09 +0530

    Commit 10/09/2026

--- OOP_Programs/Inheritance/HierarchicalDemo.java
+++ OOP_Programs/Inheritance/HierarchicalDemo.java
@@ -1,27 +1,25 @@
-import java.util.Scanner;
-
 class Vehicle {
-    String company;
+    String company="Tata Motors";
     void displayVehicle() {
         System.out.println("Company: " + company);
     }
 }
 class Car extends Vehicle {
-    int seats;
+    int seats=4;
     void displayCar() {
         displayVehicle();
         System.out.println("Car Seats: " + seats);
     }
 }
 class Bike extends Vehicle {
-    int wheels;
+    int wheels=2;
     void displayBike() {
         displayVehicle();
         System.out.println("Bike Wheels: " + wheels);
     }
 }
 class Bus extends Vehicle {
-    int capacity;
+    int capacity=50;
     void displayBus() {
         displayVehicle();
         System.out.println("Bus Capacity: " + capacity);
@@ -29,38 +27,18 @@ class Bus extends Vehicle {
 }
 public class HierarchicalDemo {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter company name (shared by all vehicles): ");
-        String company = sc.nextLine().trim();
-
-        System.out.print("Enter number of seats for Car: ");
-        int seats = sc.nextInt();
-        sc.nextLine(); // consume newline
-        System.out.print("Enter number of wheels for Bike: ");
-        int wheels = sc.nextInt();
-        sc.nextLine(); 
-        System.out.print("Enter passenger capacity for Bus: ");
-        int capacity = sc.nextInt();
-        sc.nextLine(); 
+    
 
         Car c = new Car();
-        c.company = company;
-        c.seats = seats;
-
-        Bike b = new Bike();
-        b.company = company;
-        b.wheels = wheels;
-
-        Bus bs = new Bus();
-        bs.company = company;
-        bs.capacity = capacity;
-
         System.out.println("\nCar Information:");
         c.displayCar();
+
+        Bike b = new Bike();
         System.out.println("\nBike Information:");
         b.displayBike();
+
+        Bus bs = new Bus();
         System.out.println("\nBus Information:");
         bs.displayBus();
-        sc.close();
     }
 }
