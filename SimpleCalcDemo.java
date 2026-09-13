1bc9ad4ae635749ee6c4445d20a5a4a9c5630f39
Payal <payalbute6@gmail.com>
2026-09-13 21:42:46 +0530

    Commit 13/09/2026

--- SimpleCalcDemo.java
+++ SimpleCalcDemo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 interface Calculator {
     int add(int a, int b);
     int subtract(int a, int b);
@@ -17,8 +19,18 @@ class SimpleCalc implements Calculator {
 
 public class SimpleCalcDemo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Calculator calc = new SimpleCalc();
-        System.out.println("Addition (10 + 5): " + calc.add(10, 5));
-        System.out.println("Subtraction (10 - 5): " + calc.subtract(10, 5));
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Addition (" + num1 + " + " + num2 + "): " + calc.add(num1, num2));
+        System.out.println("Subtraction (" + num1 + " - " + num2 + "): " + calc.subtract(num1, num2));
+        
+        scanner.close();
     }
 }
