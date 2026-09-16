86f4135ec35d946a4500794b34d2ce6074cd9967
Payal <payalbute6@gmail.com>
2026-09-16 18:51:48 +0530

    Commit 16/09/2026

--- Assign1/MultiplicationTables1To15.java
+++ Assign1/MultiplicationTables1To15.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class MultiplicationTables1To15 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of tables (n): ");
-        int n = sc.nextInt();
+        // System.out.print("Enter number of tables (n): ");
+        // int n = sc.nextInt();
+        int n=15;
 
-        for (int j = 1; j <= 10; j++) {
-            for (int i = 1; i <= n; i++) {
-                System.out.println(i + "*" + j + "=" + (i * j) );
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + "*" + j + "=" + (i * j) + "\t");
             }
             System.out.println();
         }
 
-        sc.close(); 
+        //sc.close(); 
     }
 }
