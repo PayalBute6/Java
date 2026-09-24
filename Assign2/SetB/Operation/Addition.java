2355870a351d10e5540f15e343f5f8ca30efbe68
Payal <payalbute6@gmail.com>
2026-09-25 00:02:26 +0530

    commmit 25/09/2026

---
+++ Assign2/SetB/Operation/Addition.java
@@ -0,0 +1,18 @@
+package Operation;
+
+/*
+ * Set B - e
+ * Write a package for Operation, which has two classes, Addition and Maximum. Addition has two 
+ * methods add () and subtract (), which are used to add two integers and subtract two, 
+ * float values respectively.
+ */
+
+public class Addition {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public float subtract(float a, float b) {
+        return a - b;
+    }
+}
