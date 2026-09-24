9e5a2c1b48de0e83ec7f057b55e0a0ffbccc20d3
Payal <payalbute6@gmail.com>
2026-09-25 00:02:33 +0530

    commmit 25/09/2026

---
+++ Assign2/SetB/Operation/Maximum.java
@@ -0,0 +1,16 @@
+package Operation;
+
+/*
+ * Set B - e
+ * Maximum has a method max () to display the maximum of two Integers 
+ */
+
+public class Maximum {
+    public void max(int a, int b) {
+        if (a > b) {
+            System.out.println("Maximum is: " + a);
+        } else {
+            System.out.println("Maximum is: " + b);
+        }
+    }
+}
